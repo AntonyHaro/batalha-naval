83dbba6c9bb803a96ab3085e18686a00e8611303
AntonyHaro <166054497+AntonyHaro@users.noreply.github.com>
2024-05-26 11:51:32 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $host = 'sql10.freemysqlhosting.net';
 $dbname = 'sql10708711';
@@ -14,3 +22,7 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
+
+</body>
+</html>
+
